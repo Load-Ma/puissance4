d3a1fd8683ee4c2739bf70704ed9dec7440fd239
Romain <romain.caille@epsi.com>
2021-03-30 14:01:33 +0200

    grille

--- puissance4/grille.cpp
+++ puissance4/grille.cpp
@@ -8,7 +8,15 @@ grille::grille(int nbpion, char nom) {
 }
 
 int grille::drawGrille() {
-	printf("Grille\n\n");
+	char tab[42] = { " " };
+
+	for (int i = 0; i < size(tab); i++)
+	{
+		printf("| %c ", tab[i]);
+		if ((i + 1) % 7 == 0 && i > 0) {
+			printf("\n");
+		}
+	}
 
 	return 0;
 }
--- puissance4/puissance4.cpp
+++ puissance4/puissance4.cpp
@@ -11,15 +11,7 @@ int main() {
 	//detectInstance->setLargeur((int*)largeur);
 	//detectInstance->setTab((int*)tab);
 
-	char tab[42] = { " " };
-
-	for (int i = 0; i < size(tab); i++)
-	{
-		printf("| %c ", tab[i]);
-		if ((i+1)%7 == 0 && i>0) {
-			printf("\n");
-		}
-	}
+	
 	return 0;
 }
 
